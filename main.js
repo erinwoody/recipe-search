d51a84ce60c6af35210cf5c9bc0eeb7d3a0d8d1e
Erin Woody <erinelizabethwoody@gmail.com>
2017-08-09 11:00:11 -0500

    changed css because js messed it up

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 var displayArea = document.querySelector(".images");
 var search = document.querySelector("#search");
+var noImage = "https://cdn4.iconfinder.com/data/icons/travel-2/500/breakfast_delicious_dinner_eat_food_fork_fun_holiday_knife_plate-512.png"
 
-let url = `http://recipepuppyproxy.herokuapp.com/api/?q=${search.value}`;
+// let url = `http://recipepuppyproxy.herokuapp.com/api/?q=${search.value}`;
 
 function ourCallback(e) {
     if (e.keyCode === 13) {
+        displayArea.innerHTML = "";
         let url = `http://recipepuppyproxy.herokuapp.com/api/?q=${search.value}`;
         fetch(url).then(function (response) {
             response.json().then(function (data) {
@@ -13,15 +15,23 @@ function ourCallback(e) {
 
                 for (var i = 0; i < results.length; i++) {
                     let imgSrc = results[i].thumbnail;
+                    let title = results[i].title;
+                    let href = results[i].href;
                     if (imgSrc === '') {
-                        displayArea.innerHTML += `<span>No Image Available</span>;`
+                        displayArea.innerHTML += `<a href ="${href}" target="_blank"><img src="https://s3-ap-southeast-1.amazonaws.com/assets.limetray.com/assets/user_images/menus/original/1442837463_Imgggg.jpg"><div class="caption">${title}</div></a>`;
+
                     } else {
-                        displayArea.innerHTML += `<img src="${imgSrc}" alt="" class="thumb">`
+                        displayArea.innerHTML += `<a href ="${href}" target="_blank"><img src=${imgSrc}><div class="caption">${title}</div></a>`;
                     }
                 }
+                search.value = "";
             });
         });
     }
 }
 
 search.addEventListener("keypress", ourCallback);
+search.addEventListener("click", function () {
+    displayArea.innerHTML = "";
+    search.value = "";
+})
